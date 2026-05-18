34b5b09f04674691a7f3ae31bbf7e6e0c16f5f36
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-05-17 23:22:51 -0500

    Icono de la aplicacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  supabaseUrl:"https://rlysjlunzgdbevgvlfpi.supabase.co",
+  supabaseKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJseXNqbHVuemdkYmV2Z3ZsZnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3Nzk2ODMsImV4cCI6MjA5NDM1NTY4M30.Gh7m-zD9vhw7vlU2D-nZKg4m8imhF-sEZUkAWSaIjlQ"
 };
